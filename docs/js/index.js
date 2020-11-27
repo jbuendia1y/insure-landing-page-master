5e6e50727353c15c14e3a5ebc101a995ce3392a2
Joaquín Buendía <jgamer669@gmail.com>
2020-11-27 08:52:06 -0500

    Activate States

--- docs/js/index.js
+++ docs/js/index.js
@@ -3,7 +3,9 @@
 var body = document.querySelector('body');
 var menu = document.getElementsByClassName('menu')[0];
 var menu__button = menu.querySelector('.box-menu__hamburger');
+var button__img = menu.querySelector('.box-menu__img');
 var menu__list = menu.querySelector('.menu__list');
+var cont = 0;
 
 var toogleStyles = function toogleStyles() {
   menu__list.classList.toggle('active__menu');
@@ -14,4 +16,12 @@ var toogleStyles = function toogleStyles() {
 
 menu__button.addEventListener('click', function () {
   toogleStyles();
+
+  if (cont == 0) {
+    button__img.src = "assets/icons/icon-close.svg";
+    cont++;
+  } else if (cont == 1) {
+    button__img.src = "assets/icons/icon-hamburger.svg";
+    cont--;
+  }
 });
--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,10 @@
 const body = document.querySelector('body')
 const menu = document.getElementsByClassName('menu')[0]
 const menu__button = menu.querySelector('.box-menu__hamburger')
+const button__img = menu.querySelector('.box-menu__img')
 const menu__list = menu.querySelector('.menu__list')
 
+let cont = 0
 
 const toogleStyles = ()=>{
     menu__list.classList.toggle('active__menu')
@@ -13,5 +15,12 @@ const toogleStyles = ()=>{
 
 menu__button.addEventListener('click',()=>{
     toogleStyles()
+    if(cont == 0){
+        button__img.src = "assets/icons/icon-close.svg"
+        cont++
+    }else if(cont == 1){
+        button__img.src = "assets/icons/icon-hamburger.svg"
+        cont--
+    }
 })
 
